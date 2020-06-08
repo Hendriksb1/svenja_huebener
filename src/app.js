d0e2809ab97d5a3db94ce4a09f57c5538b4e63ef
Hendriksb1 <hendrik.schulze.boeing@gmail.com>
2020-06-08 22:23:12 +0200

    favicons, form

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const success = $('.alert-success');
 const warning = $('.alert-warning');
 
 const showMessage = function(element) {
-  console.log(element);
   element.css('display', 'block');
   element.removeClass('hide');
   element.addClass('show');
@@ -35,56 +34,57 @@ $(document).ready(function () {
 
   ////////////////// form
 
-
   (() => {
-    const form = document.querySelector('form');
-    const formResponse = document.querySelector('#js-form-response');
 
-    form.onsubmit = e => {
-      e.preventDefault();
+    if($('body').is('.page_4')){
 
-      // Prepare data to send
-      const data = {};
-      const formElements = Array.from(form);
-      formElements.map(input => (data[input.name] = input.value));
-
-      // Log what our lambda function will receive
-
-      // data.[g-recaptcha-response]
-
-      console.log(data);
-
-        // Construct an HTTP request
-        var xhr = new XMLHttpRequest();
-        xhr.open(form.method, form.action, true);
-        xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
-        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-
-        // Send the collected data as JSON
-        xhr.send(JSON.stringify(data));
-
-        // Callback function
-        xhr.onloadend = response => {
-
-          console.log(response);
-
-          if (response.target.status === 200) {
-            // The form submission was successful
-            form.reset();
-            formResponse.innerHTML = 'Thanks for the message. I’ll be in touch shortly.';
-            showMessage(success);
-
-          } else {
-            // The form submission failed
-            formResponse.innerHTML = 'Something went wrong';
-            console.error(JSON.parse(response.target.response).message);
-            showMessage(warning);
-
-          }
-        };
-
-
-    };
+      
+          const form = document.querySelector('form');
+          const formResponse = document.querySelector('#js-form-response');
+      
+          form.onsubmit = e => {
+            e.preventDefault();
+      
+            // Prepare data to send
+            const data = {};
+            const formElements = Array.from(form);
+            formElements.map(input => (data[input.name] = input.value));
+      
+            // Log what our lambda function will receive
+      
+            // data.[g-recaptcha-response]
+      
+            console.log(data);
+      
+              // Construct an HTTP request
+              var xhr = new XMLHttpRequest();
+              xhr.open(form.method, form.action, true);
+              xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+              xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+      
+              // Send the collected data as JSON
+              xhr.send(JSON.stringify(data));
+      
+              // Callback function
+              xhr.onloadend = response => {
+      
+                console.log(response);
+      
+                if (response.target.status === 200) {
+                  // The form submission was successful
+                  form.reset();
+                  showMessage(success);
+      
+                } else {
+                  // The form submission failed
+                  // formResponse.innerHTML = 'Something went wrong';
+                  // console.error(JSON.parse(response.target.response).message);
+                  showMessage(warning);
+      
+                }
+              };
+          };
+    }
 
 
   })();
